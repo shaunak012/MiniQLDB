7df85255b5548dbd2b306481a76ae1d86d34d4c5
shaunak shukla <shaunak023@gmail.com>
2025-07-31 07:09:56 +0000

    still not working but better

--- src/models.rs
+++ src/models.rs
@@ -1,19 +1,20 @@
-use chrono::{DateTime, Utc};
+use chrono:: Utc;
 use serde::{Deserialize, Serialize};
 use serde_json::Value;
+use sha2::{Digest,Sha256};
 
-#[derive(Serialize,Deserialize,)]
+#[derive(Serialize,Deserialize)]
 pub struct LedgerEntry {
     pub id: String,
     pub data: Value,
-    pub timestamp: DateTime<Utc>,
+    pub timestamp: i64,
     pub prevhash: String,
     pub hash: String,
 }
 
 impl LedgerEntry {
-    pub fn new(id: String, data: Value, prevhash: String) -> Self {
-        let timestamp = Utc::now();
+    pub fn new(id: String, data: Value, prevhash: String) -> LedgerEntry {
+        let timestamp = Utc::now().timestamp();
         let mut entry = LedgerEntry{
             id:id,
             data:data,
@@ -22,6 +23,7 @@ impl LedgerEntry {
             hash:String::new(),
         };
         entry.hash = entry.compute_hash();
+        entry
     }
     pub fn compute_hash(&self)->String{
         let json=serde_json::json!({
@@ -29,6 +31,10 @@ impl LedgerEntry {
             "data":self.data,
             "timestamp":self.timestamp,
             "prevhash":self.prevhash,
-        })
+        });
+        let serialized= serde_json::to_string(&json).unwrap();
+        let mut hasher = Sha256::now();
+        hasher.update(serialized);
+        format!("{:x}",hasher.finalize())
     }
 }
