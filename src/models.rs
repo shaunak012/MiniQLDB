65c93bb552a40df70eeb5bc43d5fc4e3e2ee2ace
shaunak shukla <shaunak023@gmail.com>
2025-07-31 07:05:30 +0000

    added some functions to struct (not working currently)

--- src/models.rs
+++ src/models.rs
@@ -1,10 +1,34 @@
+use chrono::{DateTime, Utc};
+use serde::{Deserialize, Serialize};
 use serde_json::Value;
 
-pub struct LedgerEntry{
+#[derive(Serialize,Deserialize,)]
+pub struct LedgerEntry {
     pub id: String,
     pub data: Value,
-    pub timestamp: i64,
+    pub timestamp: DateTime<Utc>,
     pub prevhash: String,
-    pub hash: String
+    pub hash: String,
 }
 
+impl LedgerEntry {
+    pub fn new(id: String, data: Value, prevhash: String) -> Self {
+        let timestamp = Utc::now();
+        let mut entry = LedgerEntry{
+            id:id,
+            data:data,
+            timestamp:timestamp,
+            prevhash:prevhash,
+            hash:String::new(),
+        };
+        entry.hash = entry.compute_hash();
+    }
+    pub fn compute_hash(&self)->String{
+        let json=serde_json::json!({
+            "id":self.id,
+            "data":self.data,
+            "timestamp":self.timestamp,
+            "prevhash":self.prevhash,
+        })
+    }
+}
