7c41550c89f0946ce3f016042e861d7567abdb44
shaunak shukla <shaunak023@gmail.com>
2025-07-31 07:15:51 +0000

    it works now \o/

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,16 @@
+use serde_json::json;
+
+use crate::models::LedgerEntry;
+
 mod cli;
 mod ledger;
 mod models;
 
 fn main() {
-    println!("Hello, world!");
-    cli::run();
+    let entry = LedgerEntry::new(
+        "user".to_string(),
+        json!({"name":"Shaunak","coins":500}),
+        "0".to_string(),
+    );
+    println!("{:#?}", entry);
 }
--- src/models.rs
+++ src/models.rs
@@ -1,9 +1,9 @@
-use chrono:: Utc;
+use chrono::Utc;
 use serde::{Deserialize, Serialize};
 use serde_json::Value;
-use sha2::{Digest,Sha256};
+use sha2::{Digest, Sha256};
 
-#[derive(Serialize,Deserialize)]
+#[derive(Serialize, Deserialize,Debug)]
 pub struct LedgerEntry {
     pub id: String,
     pub data: Value,
@@ -15,26 +15,26 @@ pub struct LedgerEntry {
 impl LedgerEntry {
     pub fn new(id: String, data: Value, prevhash: String) -> LedgerEntry {
         let timestamp = Utc::now().timestamp();
-        let mut entry = LedgerEntry{
-            id:id,
-            data:data,
-            timestamp:timestamp,
-            prevhash:prevhash,
-            hash:String::new(),
+        let mut entry = LedgerEntry {
+            id: id,
+            data: data,
+            timestamp: timestamp,
+            prevhash: prevhash,
+            hash: String::new(),
         };
         entry.hash = entry.compute_hash();
         entry
     }
-    pub fn compute_hash(&self)->String{
-        let json=serde_json::json!({
+    pub fn compute_hash(&self) -> String {
+        let json = serde_json::json!({
             "id":self.id,
             "data":self.data,
             "timestamp":self.timestamp,
             "prevhash":self.prevhash,
         });
-        let serialized= serde_json::to_string(&json).unwrap();
-        let mut hasher = Sha256::now();
+        let serialized = serde_json::to_string(&json).unwrap();
+        let mut hasher = Sha256::new();
         hasher.update(serialized);
-        format!("{:x}",hasher.finalize())
+        format!("{:x}", hasher.finalize())
     }
 }
