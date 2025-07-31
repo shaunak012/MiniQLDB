08827b884f29077df9094ac58fbeba42f857c2e1
shaunak shukla <shaunak023@gmail.com>
2025-07-31 10:46:50 +0000

    it works \o/ (create and read now possible)

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,24 @@
-use serde_json::json;
-
-use crate::models::LedgerEntry;
-
 mod cli;
 mod ledger;
 mod models;
 
+use serde_json::json;
+use models::LedgerEntry;
+use crate::ledger::{append_entry, read_all_entries, get_last_hash};
+
 fn main() {
+    let prev_hash = get_last_hash().unwrap();
     let entry = LedgerEntry::new(
         "user".to_string(),
-        json!({"name":"Shaunak","coins":500}),
-        "0".to_string(),
+        json!({"name":"Shaunak2","coins":500}),
+        prev_hash,
     );
     println!("{:#?}", entry);
+    
+    append_entry(&entry).unwrap();
+    println!("All ledger Entries:");
+    let all_entries = read_all_entries().unwrap();
+    for entry in all_entries{
+        println!("{:#?}",entry);
+    }
 }
