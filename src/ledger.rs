25d61b88ff9a44b5487fbe291301fa894cfaa138
shaunak shukla <shaunak023@gmail.com>
2025-07-31 10:02:55 +0000

    added 2 functions (broken currently

--- src/ledger.rs
+++ src/ledger.rs
@@ -12,3 +12,17 @@ pub fn append_entry(entry: &LedgerEntry) -> std::io::Result<()>{
     writeln!(file,"{}",json_line);
     Ok(())
 }
+
+pub read_all_entries() -> std::io::Result<Vec<LedgerEntry>>{
+    //Todo
+}
+
+
+pub fn get_last_hash() -> std::io::Result<String>{
+    let entries = read_all_entries();
+    if let Some(last) = entries.last(){
+        Ok(last.hash.clone())
+    } else {
+        Ok("0".to_string())
+    }
+}
