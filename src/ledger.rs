d29c3959fd07ee4d657659d30de0fbf91b88fc4f
shaunak shukla <shaunak023@gmail.com>
2025-07-31 09:55:55 +0000

    added function to append entry

--- src/ledger.rs
+++ src/ledger.rs
@@ -1,1 +1,14 @@
-// for ledger operations
+// for ledger operation
+
+use std::fs::{OpenOptions, File};
+use std::io::{Write};
+use crate::models::LedgerEntry;
+
+const LEDGER_FILE: &str= "ledger.jsonl";
+
+pub fn append_entry(entry: &LedgerEntry) -> std::io::Result<()>{
+    let mut file = OpenOptions::new().create(true).append(true).open(LEDGER_FILE)?;
+    let json_line=serde_json::to_string(entry)?;
+    writeln!(file,"{}",json_line);
+    Ok(())
+}
