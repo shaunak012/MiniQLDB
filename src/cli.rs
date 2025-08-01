4dd0cf6f4ad6bf91c10299cf76e405355a1c0689
shaunak shukla <shaunak023@gmail.com>
2025-08-01 09:13:08 +0000

    started making api

--- src/cli.rs
+++ src/cli.rs
@@ -1,3 +1,23 @@
-pub fn run(){
-    println!("Hello World!")
+use clap::{Parser};
+use crate::models::LedgerEntry;
+use crate::ledger::{append_entry, get_last_hash, read_all_entries};
+
+
+#[derive(Parser)]
+#[command(name="MiniQLDB", version="0.1", about="Lightweight, imutable ledger")]
+pub struct cli{
+    #[command(subcommand)]
+    pub command:Commands,
+}
+
+pub enum Commands{
+    Add{
+        
+    },
+    Get{
+
+    },
+    History{
+
+    },
 }
